28b5f2d53ea8ab8dd044f4994c07541dcf47fc54
Lisa <119270950+sthrmzy@users.noreply.github.com>
2023-12-04 10:45:14 -0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,14 +1,23 @@
 function mostrarSecao(id) {
     // Oculta todas as seções
     var secoes = document.querySelectorAll('.secao');
-    secoes.forEach(function(secao) {
-        secao.style.display = 'none';
+    secoes.forEach(function (secao) {
+        secao.classList.remove('mostrar');
+        setTimeout(function () {
+            secao.style.display = 'none';
+        }, 500); // Aguarda o término da transição (500ms)
     });
 
     // Mostra a seção clicada
     var secaoSelecionada = document.getElementById(id);
     if (secaoSelecionada) {
+        secaoSelecionada.style.opacity = '0';
         secaoSelecionada.style.display = 'block';
+
+        // Ativa a animação de desvanecimento
+        setTimeout(function () {
+            secaoSelecionada.style.opacity = '1';
+            secaoSelecionada.classList.add('mostrar');
+        }, 50); // Adiciona a classe após um pequeno intervalo para garantir que a transição ocorra
     }
 }
-
